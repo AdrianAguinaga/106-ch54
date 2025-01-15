ecb766046bf7b0ae548efbe908b415b253dc5f96
nefersillo <adrian.aguinaga@uabc.edu.mx>
2025-01-14 19:34:57 -0800

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,16 @@
+function sayHello(){
+    console.log("Hello");
+}
+
+function init(){
+    let a = "new";
+    console.log("hello world!");
+    sayHi("Adrian");
+    sayHello();
+}
+function sayHi(name){
+    console.log("hi "+ name)
+}
+
+winwow.onload = init;
+//Variable Scope
